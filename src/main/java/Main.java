3d3bc6eccd6edae2d40deadec6d4db067c81c1b1
David Cereteli <david.cereteli@gmail.com>
2021-01-04 13:31:38 +0100

    Add Main to test Range

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Range range1 = new Range();
+        Range range2 = new Range();
+        range1.start = 1;
+        range1.end = 2;
+        range2.start = 3;
+        range2.end = 4;
+    }
+}
