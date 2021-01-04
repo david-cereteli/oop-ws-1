61db6f338436cd6328ef88a2263327306721d63c
David Cereteli <david.cereteli@gmail.com>
2021-01-04 14:01:51 +0100

    Add constructor, setter, getter that prevent invalid ranges

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-        Range range1 = new Range();
-        Range range2 = new Range();
-        range1.start = 1;
-        range1.end = 2;
-        range2.start = 3;
-        range2.end = 4;
+        Range range1 = new Range(1, 10);
+        Range range2 = new Range(2, 1);
+        System.out.println(range1);
+        System.out.println(range2);
+       // System.out.println(range1.getStart());
+       // System.out.println(range1.getEnd());
+       // System.out.println(range2.getStart());
+       // System.out.println(range2.getEnd());
+        range1.setEnd(2);
+        range1.setStart(1);
+        range2.setEnd(4);
+        range2.setStart(3);
+        System.out.println(range1.getStart());
+        System.out.println(range1.getEnd());
+        System.out.println(range2.getStart());
+        System.out.println(range2.getEnd());
+        range1.setStart(6);
+        range2.setEnd(5);
+        System.out.println(range1.getStart());
+        System.out.println(range1.getEnd());
+        System.out.println(range2.getStart());
+        System.out.println(range2.getEnd());
     }
 }
--- src/main/java/Range.java
+++ src/main/java/Range.java
@@ -1,4 +1,42 @@
 public class Range {
-    public int start;
-    public int end;
+    private int start;
+    private int end;
+
+    public Range() {
+    }
+
+    public Range(int start, int end) {
+        if (start <= end) {
+            this.start = start;
+            this.end = end;
+        } else {
+            this.start = end;
+            this.end = start;
+        }
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        if (start <= this.end) {
+            this.start = start;
+        }
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        if (this.start <= end) {
+            this.end = end;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Start: %d End: %d", this.start, this.end);
+    }
 }
